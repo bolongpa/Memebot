d0dca06b9273bca0159c29d523b4554ad9f8342a
Bolong Pan <43298067+bolongpa@users.noreply.github.com>
2020-10-26 10:32:41 -0700

    Add files via upload

---
+++ dialogue_data/movie_dialogue_extractor.py
@@ -0,0 +1,25 @@
+"""python script to reorganize the dialogues from movies. results put into csv file delimited by semicolon."""
+
+import csv
+
+with open('extracted_dialogue.txt', 'w+') as f:
+
+    with open('movie_lines.txt', 'r', encoding="utf-8", errors="ignore") as f1:
+        lines1 = f1.readlines()
+        line_dic = {}
+        for i, l in enumerate(lines1):
+            s = l.split(' +++$+++ ')
+            line_dic[s[0]] = s[-1].strip('\n')
+
+        with open('movie_conversations.txt', 'r', encoding="utf-8") as f2:
+            lines2 = f2.readlines()
+            dialogue_code = []
+            for i, s in enumerate(lines2):
+                temp = lines2[i].split(' +++$+++ ')[-1].strip('\n[]\'').split('\', \'')
+                for j in range(len(temp)-1):
+                    dialogue_code.append([temp[j], temp[j+1]])
+
+            # extract dialogue to file
+            for p in dialogue_code:
+                f.write(line_dic[p[0]]+'\n')
+                f.write(line_dic[p[1]]+'\n')
