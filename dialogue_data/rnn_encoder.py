3aa8a1ce66bf318b7ce9e1a0ea2bb285027decba
xsy1996 <Xushiyu1996@gmail.com>
2020-11-02 00:31:35 -0800

    update

--- dialogue_data/rnn_encoder.py
+++ dialogue_data/rnn_encoder.py
@@ -15,6 +15,8 @@ SOS_token = 0
 EOS_token = 1
 learning_rate = 0.1
 hidden_size = 256
+teacher_forcing_ratio = 0.5
+
 
 
 class Lang:
@@ -54,37 +56,125 @@ class EncoderRNN(nn.Module):
     def initHidden(self):
         return torch.zeros(1, 1, self.hidden_size, device=device)
 
+class DecoderRNN(nn.Module):
+    def __init__(self, hidden_size, output_size):
+        super(DecoderRNN, self).__init__()
+        self.hidden_size = hidden_size
+
+        self.embedding = nn.Embedding(output_size, hidden_size)
+        self.gru = nn.GRU(hidden_size, hidden_size)
+        self.out = nn.Linear(hidden_size, output_size)
+        self.softmax = nn.LogSoftmax(dim=1)
+
+    def forward(self, input, hidden):
+        output = self.embedding(input).view(1, 1, -1)
+        output = F.relu(output)
+        output, hidden = self.gru(output, hidden)
+        output = self.softmax(self.out(output[0]))
+        return output, hidden
+
+    def initHidden(self):
+        return torch.zeros(1, 1, self.hidden_size, device=device)
+
 def prepareData():
-	lang_dict = Lang()
-	sentence = []
-	reply = []
-	with open("extracted_dialogue.csv","r") as f:
-		lines = f.read().strip().split("\n")
-		for line in lines:
-			sentence.append(line.split(",")[0])
-			lang_dict.addSentence(line.split(",")[0])
-	return lang_dict, sentence, reply
+    lang_dict = Lang()
+    sentences = []
+    max_length=0
+    with open("extracted_dialogue.txt","r") as f:
+        lines = f.read().strip().split("\n")
+        for i in range(0, len(lines), 2):
+            tmp = lines[i].strip()
+            sentences.append(tmp)
+            lang_dict.addSentence(tmp)
+            if len(tmp) > max_length:
+                max_length = len(tmp)
+    return lang_dict, sentences, max_length
 
 def getSentenceTensor(lang, sentence):
     indexes = [lang.word2index[word] for word in sentence.split(' ')]
     indexes.append(EOS_token)
     return torch.tensor(indexes, dtype=torch.long, device=device).view(-1, 1)
 
+def train(input_tensor, target_tensor, encoder, decoder, encoder_optimizer, decoder_optimizer, criterion, max_length):
+    
+    encoder_hidden = encoder.initHidden() 
 
-lang_dict, sentence, reply = prepareData()
+    encoder_optimizer.zero_grad()
+    decoder_optimizer.zero_grad()
+
+    input_length = input_tensor.size(0)
+    target_length = target_tensor.size(0)
+
+    encoder_outputs = torch.zeros(max_length, encoder.hidden_size, device=device)
+
+    loss = 0
+
+    for ei in range(input_length):
+        encoder_output, encoder_hidden = encoder(
+            input_tensor[ei], encoder_hidden)
+        encoder_outputs[ei] = encoder_output[0, 0]
+
+    decoder_input = torch.tensor([[SOS_token]], device=device)
+
+    decoder_hidden = encoder_hidden
+
+    use_teacher_forcing = True if random.random() < teacher_forcing_ratio else False
+
+    if use_teacher_forcing:
+        for di in range(target_length):
+            decoder_output, decoder_hidden = decoder(
+                decoder_input, decoder_hidden)
+            loss += criterion(decoder_output, target_tensor[di])
+            decoder_input = target_tensor[di]  
+
+    else:
+        for di in range(target_length):
+            decoder_output, decoder_hidden = decoder(
+                decoder_input, decoder_hidden)
+            topv, topi = decoder_output.topk(1)
+            decoder_input = topi.squeeze().detach() 
+
+            loss += criterion(decoder_output, target_tensor[di])
+            if decoder_input.item() == EOS_token:
+                break
+
+    loss.backward()
+
+    encoder_optimizer.step()
+    decoder_optimizer.step()
+
+    return loss.item() / target_length
+
+
+def trainIters(encoder, decoder, lang_dict, max_length, sentences,  n_iters, print_every=1000, learning_rate=0.01):
+    print_loss_total = 0
+    
+    encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
+    decoder_optimizer = optim.SGD(decoder.parameters(), lr=learning_rate)
+    training_data = [getSentenceTensor(lang_dict, random.choice(sentences))
+                      for i in range(n_iters)]
+    criterion = nn.NLLLoss()
+
+    for iter in range(1, n_iters + 1):
+        iter_data = training_data[iter - 1]
+        input_tensor = iter_data
+        target_tensor = iter_data
+
+        loss = train(input_tensor, target_tensor, encoder,
+                     decoder, encoder_optimizer, decoder_optimizer, criterion, max_length)
+        print_loss_total += loss
+
+        if iter % print_every == 0:
+            print_loss_avg = print_loss_total / print_every
+            print_loss_total = 0
+            print('iter, loss =  (%d %.4f) ' %  (iter,  print_loss_avg))
+
+
+
+lang_dict, sentences, max_length = prepareData()
 
 encoder = EncoderRNN(lang_dict.n_words, hidden_size).to(device)
-encoder_optimizer = optim.SGD(encoder.parameters(), lr=learning_rate)
-criterion = None
+decoder = DecoderRNN(hidden_size, lang_dict.n_words).to(device)
 
-for i in range(10):
-	input_tensor = getSentenceTensor(sentence[i])
-	target_tensor = getSentenceTensor(reply[i])
-	encoder_hidden = encoder.initHidden()
+trainIters(encoder, decoder, lang_dict, max_length, sentences, 1000, print_every=50)
 
-	encoder_optimizer.zero_grad()
-
-	input_length = input_tensor.size(0)
-	for i in range(input_length):
-		encode_code, encoder_hidden = encoder(input_tensor[i], encoder_hidden)
-	print(encode_code, encoder_hidden)
